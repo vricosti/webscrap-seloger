06250fa0b1a9589f05d9d52f6d4e80ec25ce9c9d
Vince Ricosti <80467769+vricosti@users.noreply.github.com>
2024-06-15 16:31:37 +0200

    playwright wip

--- playwright/index.js
+++ playwright/index.js
@@ -1,25 +1,27 @@
 // Import the Playwright library to use it
 const playwright = require("playwright");
+const fs = require('fs');
 
 (async () => {
   // Launch a new instance of a Chromium browser
-  const browser = await playwright.chromium.launch({
+  const browser = await playwright.firefox.launch({
     // Set headless to false so you can see how Playwright is
     // interacting with the browser
     headless: false,
+    args: ["-fullscreen"],
   });
   // Create a new Playwright context
   const context = await browser.newContext();
-  // Create a new page/tab in the context.
   const page = await context.newPage();
 
-  // Navigate to the Bright Data home page.
   let url ='https://www.seloger.com'
   await page.goto(url);
+
+  await page.context().storageState({ path: 'data/data.json' });
 
   // Wait 10 seconds (or 10,000 milliseconds)
   await page.waitForTimeout(10000);
 
   // Close the browser
-  await browser.close();
+  await context.close();
 })();
